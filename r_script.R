1ba4548879fa5cd7fff60dc12a13efce51133854
hrgodmann <hrgodmann@gmail.com>
2023-12-23 19:54:38 +0100

    add changes

--- r_script.R
+++ r_script.R
@@ -2,7 +2,7 @@
 ######################################## Appendix Script ########################################
 
 # R-Script for:
-# Interrupted Time Series: A Comparison Of Different Methods With Theoretical And Practical Considerations
+# Interrupted Time Series
 # Uni Assignment For -Data Science In Clinical Practice-
 # Henrik Godmann (13802453)
 
@@ -115,6 +115,7 @@ dat2 <- dat2 %>%
 
 colnames(dat2)[3] <- "dep"
 
+
 # We can also test stationarity with a hypothesis test: (Dickey-Fuller test)
 adf.test(dat1$dep, alternative = "stationary")
 # significant with all measures, not significant with daily measures, so the time series rather needs differencing
@@ -185,7 +186,7 @@ abline(v=as.Date("2012-11-19"), lty=2, lwd=1.5)
 # dev.off()
 
 
-####### mood predition (see Appendix)
+####### mood prediction
 
 imputed2$mood_mean <- rowMeans(imputed2[c(8,9,11,12,14,16,17)])
 names(imputed2)
@@ -228,32 +229,38 @@ axis(2, las = 1, cex.axis = 1.2)
 box(lwd=2)
 
 
-#fit autom- arima to mood data
-fit2 <- auto.arima(datM1$mood_mean, d = 0, seasonal = FALSE)
+
+prepare <- rbind(datM1, datM2)
+
+training <- prepare[c(1:230),]
+test <- prepare[c(231:nrow(prepare)),]
+
+
+
+
+
+
+#fit auto. arima to mood data
+fit2 <- auto.arima(training$mood_mean, d = 0, seasonal = FALSE)
 summary(fit2)
 
-forecasts <- forecast(fit2, h = nrow(datM2))
+forecasts <- forecast(fit2, h = nrow(test))
 
 # Prepare data for plotting all (skip if you want to plot daily)
-date <- c(datM1$date[500:671], datM2$date[1:30])
-actual_data <- c(datM1$mood_mean[500:671], datM2$mood_mean[1:30])
-predicted <- c(rep(NA, length(datM1$mood_mean[500:671])), forecasts$mean[1:30])
-lower_bound <- c(rep(NA, length(datM1$mood_mean[500:671])), forecasts$lower[, "80%"][1:30])
-upper_bound <- c(rep(NA, length(datM1$mood_mean[500:671])), forecasts$upper[, "80%"][1:30])
-
-# Prepare data for plotting daily
-date <- c(datM1$date[60:98], datM2$date[1:10])
-actual_data <- c(datM1$mood_mean[60:98], datM2$mood_mean[1:10])
-predicted <- c(rep(NA, length(datM1$mood_mean[60:98])), forecasts$mean[1:10])
-lower_bound <- c(rep(NA, length(datM1$mood_mean[60:98])), forecasts$lower[, "80%"][1:10])
-upper_bound <- c(rep(NA, length(datM1$mood_mean[60:98])), forecasts$upper[, "80%"][1:10])
-
+date <- c(training$date[150:230], test$date[1:8])
+actual_data <- c(training$mood_mean[150:230], datM3$mood_mean[1:8])
+predicted <- c(rep(NA, length(training$mood_mean[150:230])), forecasts$mean[1:8])
+lower_bound <- c(rep(NA, length(training$mood_mean[150:230])), forecasts$lower[, "95%"][1:8])
+upper_bound <- c(rep(NA, length(training$mood_mean[150:230])), forecasts$upper[, "95%"][1:8])
 
 
 df <- data.frame(date, actual_data, predicted, lower_bound, upper_bound)
 
-# setwd("/Users/henrikgodmann/Desktop/workspace/semester_5/Clinical_Data_Science/final_assignment/plots")
-# png("arima_mood.png", width=12*300, height=8*300, res=300)
+
+
+
+setwd("/Users/henrikgodmann/Desktop/workspace/semester_5/Clinical_Data_Science/final_assignment/plots")
+png("final_arima.png", width=12*300, height=8*300, res=300)
 # Plot
 plot(df$date, df$actual_data, type = "l", col = my_blue, lwd = 2,
      xlab = "", ylab = "",
@@ -273,9 +280,17 @@ lines(df$date, df$upper_bound, col = my_red_strong, lwd = 2, lty = 2)
 legend("topleft", legend = c("Actual Data", "Predicted", "80% Confidence Interval"),
        col = c(my_blue, my_red, my_red_strong), lty = c(1, 1, 2), cex = 0.8, lwd = c(2, 3, 2))
 box(lwd = 2)
-abline(v=as.Date("2012-11-19"), lty=2, lwd=1.5)
+abline(v=as.Date("2013-04-01"), lty=2, lwd=1.5)
 
-# dev.off()
+dev.off()
+
+
+
+
+
+
+
+
 
 
 
@@ -420,31 +435,33 @@ write.csv(datML,"/Users/henrikgodmann/Desktop/workspace/semester_5/Clinical_Data
 
 ####### CLEAR ENVIRONMENT AND RUN PREPARE DATA SECTION AGAIN FIRST
 
-imputed2_cut <- imputed2[c(2,69:82)]
-datSSM1 <- cbind(dat1[1], imputed2_cut)
-datSSM1$phase <- 0
 
-imputed3_cut <- imputed3[c(2,69:82)]
+
+imputed2_cut <- imputed2[c(2,8,9,11,12,14,16,17,69:82)]
+datSSM1 <- cbind(dat1[1], imputed2_cut)
+
+
+imputed3_cut <- imputed3[c(2,8,9,11,12,14,16,17,69:82)]
 datSSM2 <- cbind(dat2[1], imputed3_cut)
-datSSM2$phase <- 1
+
 
 datSSM <- rbind(datSSM1,datSSM2)
 
-head(datSSM)
+names(datSSM)
+
+datSSM$neg_mood <- rowMeans(datSSM[c(3:9)])
+
 
 library(dplyr)
 library(KFAS)
 
-# For State Space Models, we will aggregate the data to daily measures
-daily_data <- datSSM %>%
-  group_by(date) %>%
-  summarise(across(starts_with("dep"), mean, na.rm = TRUE),
-            phase = mean(phase, na.rm = TRUE))
+
+daily_data <- datSSM[-c(3:22)]
 
 
-ssm_model <- SSModel(daily_data$dep ~ SSMtrend(1, Q = list(matrix(NA))) + 
-                       SSMregression(~ phase, data = daily_data, Q = matrix(NA)), 
-                     H = matrix(NA))
+ssm_model <- SSModel(daily_data$dep ~ SSMtrend(1, Q = list(matrix(NA))) +
+SSMregression(~ phase, data = daily_data, Q = matrix(NA)),
+H = matrix(NA))
 
 
 fit <- fitSSM(ssm_model, inits = rep(0, length = 3), method = "BFGS")# Fit the model
@@ -460,36 +477,26 @@ print(smoothed_states)
 df <- as.data.frame(smoothed_states)
 
 
-# setwd("/Users/henrikgodmann/Desktop/workspace/semester_5/Clinical_Data_Science/final_assignment/plots")
-# png("phase_ssm.png", width=12*300, height=8*300, res=300)
-plot(daily_data$date, df$phase[-c(1)], type = "l", col = my_red, lwd = 3,
-     xlab = "", ylab = "", xaxt = "n", yaxt = "n")
-
-title(xlab = "Time", cex.lab = 1)
-title(ylab = "State Estimate", cex.lab = 1)
-month_dates <- seq(from = min(dat3$date), to = max(dat3$date), by = "month")
-axis(1, at = month_dates, labels = format(month_dates, "%b %Y"), cex.axis = 1.2, las = 1)
-axis(2, las = 1, cex.axis = 1.2)
-box(lwd=2)
-# dev.off()
-
-
-# setwd("/Users/henrikgodmann/Desktop/workspace/semester_5/Clinical_Data_Science/final_assignment/plots")
-# png("ssm_results.png", width=8*300, height=8*300, res=300)
+setwd("/Users/henrikgodmann/Desktop/workspace/semester_5/Clinical_Data_Science/final_assignment/plots")
+png("final_ssm2.png", width=8*300, height=8*300, res=300)
 par(mfrow = c(2,1))
 
 plot(daily_data$date, daily_data$dep, type = "l", col = my_red, lwd = 2,
      xlab = "", ylab = "", xaxt = "n", yaxt = "n")
 
-title(main = "Depression Score An Estimated States Over Time", font.main = 2, cex.main = 1)
+title(main = "Depression Score And Estimated States Over Time", font.main = 2, cex.main = 1)
 title(xlab = "Time", cex.lab = 1)
 title(ylab = "Depression Score", cex.lab = 1)
 month_dates <- seq(from = min(dat3$date), to = max(dat3$date), by = "month")
 axis(1, at = month_dates, labels = format(month_dates, "%b %Y"), cex.axis = 1.2, las = 1)
 axis(2, las = 1, cex.axis = 1.2)
 box(lwd=2)
+# abline(v = daily_data$date[177], col = my_blue, lty = 2, lwd=2) 
+# abline(v = daily_data$date[287], col = my_blue, lty = 2, lwd=2) 
+abline(v = daily_data$date[672], col = my_blue, lty = 2, lwd=2) 
+# abline(v = daily_data$date[990], col = my_blue, lty = 2, lwd=2) 
 
-plot(daily_data$date, smoothed_states[-1], type = "l", col = my_red, lwd = 2,
+plot(daily_data$date[-c(1:4)], df$level[-c(1:5)], type = "l", col = my_red, lwd = 2,
      xlab = "", ylab = "", xaxt = "n", yaxt = "n")
 
 # title(main = "Depression Score Over Time", font.main = 2, cex.main = 1)
@@ -501,14 +508,13 @@ axis(2, las = 1, cex.axis = 1.2)
 box(lwd=2)
 
 
-abline(v = daily_data$date[99], col = my_blue, lty = 2, lwd=2) 
+# abline(v = daily_data$date[177], col = my_blue, lty = 2, lwd=2) 
+# abline(v = daily_data$date[287], col = my_blue, lty = 2, lwd=2) 
+abline(v = daily_data$date[672], col = my_blue, lty = 2, lwd=2) 
+# abline(v = daily_data$date[990], col = my_blue, lty = 2, lwd=2) 
 
 
-# dev.off()
-
-
-
-
+dev.off()
 
 
 
